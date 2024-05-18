d1acbc1fd8368b59fbd56d09e4484c3761942c10
CledsonFirmino <sistemas2014@gmail.com>
2024-05-18 00:48:51 -0300

    Clientes funcionando, Pets e vendas a corrigir

--- app_pet/models.py
+++ app_pet/models.py
@@ -33,7 +33,7 @@ OPCOES_PORTE = (
     (1, "Pequeno 6 a 15kg"),
     (2, "Médio 15 a 25kg"),
     (3, "Grande 25 a 45kg"),
-    (4, ",Gigantes 45 a 90kg"),
+    (4, ",Gigante 45 a 90kg"),
 )
 
 OPCOES_PELAGEM = (
@@ -47,6 +47,20 @@ OPCOES_PELAGEM = (
     (8, "Textura lisa"),
 )
 
+OPCOES_BANHO = (
+    (1, "Simples"),
+    (2, "Composto"),
+    (3, "Hidratação"),
+)
+
+OPCOES_TOSA = (
+    (1, "Tosa Higiênica"),
+    (2, "Tosa na Tesoura"),
+    (3, "Tosa Verão"),
+    (4, "Tosa na Máquina"),
+    (5, "Tosa Bebê"),
+    (6, "Tosa da Raça"),
+)
 # DADOS DO CLIENTE
 class Cadastro_cliente(models.Model):
     # Cliente fez compra de objetos, pet ou serviços pode ou não ter pet
@@ -76,72 +90,12 @@ class Cadastro_pet(models.Model):
 # DADOS DE SERVIÇO
 # Obs.: Deve constar o nome do Cliente
 # e seu pet ou vários pets
-
-OPCOES_RACA = (
-    (1, "Border Collie"),
-    (2, "Buldogue francês"),
-    (3, "Buldogue inglês"),
-    (4, "Chihuahua"),
-    (5, "Dachshund"),
-    (6, "Fila Brasileiro"),
-    (7, "Fox Terrier"),
-    (8, "Golden Retriever"),
-    (9, "Husky"),
-    (10, "Labrador"),
-    (11, "Lhasa Apso"),
-    (12, "Maltês"),
-    (13, "Pinscher"),
-    (14, "Poodle"),
-    (15, "Pug"),
-    (16, "Pastor"),
-    (17, "Rottweiler"),
-    (18, "Schnauzer"),
-    (19, "Terrier"),
-    (20, "Yorkshire"),
-    (21, "Shih Tzu"),
-    (22, "Schnauzer"),
-    (23, "SRD (Sem Raça Definida)"),
-)
-
-OPCOES_PORTE = (
-    (1, "Pequeno 6 a 15kg"),
-    (2, "Médio 15 a 25kg"),
-    (3, "Grande 25 a 45kg"),
-    (4, ",Gigantes 45 a 90kg"),
-)
-
-OPCOES_PELAGEM = (
-    (1, "Longa"),
-    (2, "Curta"),
-    (3, "Encaracolado"),
-    (4, "Dupla"),
-    (5, "Peculiar"),
-    (6, "Longo Ondulado"),
-    (7, "Textura dura"),
-    (8, "Textura lisa"),
-)
-
-OPCOES_BANHO = (
-    (1, "Simples"),
-    (2, "Composto"),
-    (3, "Hidratação"),
-)
-
-OPCOES_TOSA = (
-    (1, "Tosa Higiênica"),
-    (2, "Tosa na Tesoura"),
-    (3, "Tosa Verão"),
-    (4, "Tosa na Máquina"),
-    (5, "Tosa Bebê"),
-    (6, "Tosa da Raça"),
-)
-
 class Cadastro_venda(models.Model):
     # Cliente faz compra de objetos, pets ou serviço
     # Nome do Cliente e nome do Pet a qual foi solicitado o serviço
     cliente_compra = models.ForeignKey(Cadastro_cliente, on_delete=models.DO_NOTHING)
     pet_cliente = models.ForeignKey(Cadastro_pet, on_delete=models.DO_NOTHING)
-    banho_tipo = models.CharField(max_length=50, choices=OPCOES_BANHO)
+    banho_tipo = models.CharField(max_length=50, choices=OPCOES_BANHO, default="1")
     valor_banho = models.FloatField()
     tosa_tipo = models.CharField(max_length=50, choices=OPCOES_TOSA, default="1")
     valor_tosa = models.FloatField()
--- app_pet/views.py
+++ app_pet/views.py
@@ -7,170 +7,166 @@ from .models import Cadastro_venda
 from .forms import CadastroVendaForm
 from django.contrib.auth.decorators import login_required
 
-#---------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------
 # LÓGICA DE CADASTRO DOS CLIENTES
-#---------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------
 
-def cliente (request):
-    dados_cliente = {
-        'dados_cliente': Cadastro_cliente.objects.all()
-    }
-    return render (request, 'cad_clientes/clientes.html',context=dados_cliente)
 
-def detalhe_cliente (request ,id_cliente):
-    dados_c = {
-        'dados':Cadastro_cliente.objects.get(pk=id_cliente)
-    }
-    return render (request,'cad_clientes/detalhe_cliente.html', dados_c)
+def cliente(request):
+    dados_cliente = {"dados_cliente": Cadastro_cliente.objects.all()}
+    return render(request, "cad_clientes/clientes.html", context=dados_cliente)
+
+
+def detalhe_cliente(request, id_cliente):
+    dados_c = {"dados": Cadastro_cliente.objects.get(pk=id_cliente)}
+    return render(request, "cad_clientes/detalhe_cliente.html", dados_c)
+
 
 @login_required
-def criar_cliente (request):
-    if request.method == 'POST':
+def criar_cliente(request):
+    if request.method == "POST":
         form_cliente = CadastroClienteForm(request.POST)
         if form_cliente.is_valid():
             form_cliente.save()
-            return redirect('cliente')
+        return redirect("cliente")
     else:
         cliente_form = CadastroClienteForm()
-        form_cliente = {
-        'form_cliente': cliente_form             
-        }
-        return render (request, 'cad_clientes/novo_cliente.html', context=form_cliente)
+        form_cliente = {"form_cliente": cliente_form}
+    return render(request, "cad_clientes/novo_cliente.html", context=form_cliente)
 
-@login_required    
-def editar_cliente(request,id_cliente):
+
+@login_required
+def editar_cliente(request, id_cliente):
     cliente = Cadastro_cliente.objects.get(pk=id_cliente)
-            #novo_cadastro/1 -> GET
-    if request.method == 'GET':
-            form_cliente = CadastroClienteForm(instance=cliente)
-            return render (request, 'cad_clientes/novo_cliente.html', {'form_cliente': form_cliente})
-            #caso requisição seja POST
+    # novo_cadastro/1 -> GET
+    if request.method == "GET":
+        form_cliente = CadastroClienteForm(instance=cliente)
+        return render(
+            request, "cad_clientes/novo_cliente.html", {"form_cliente": form_cliente}
+        )
+        # caso requisição seja POST
     else:
-        form_cliente =  CadastroClienteForm(request.POST, instance=cliente)
+        form_cliente = CadastroClienteForm(request.POST, instance=cliente)
         if form_cliente.is_valid():
-               form_cliente.save()
-        return redirect('cliente') 
-    
-@login_required    
-def excluir_cliente (request,id_cliente):
+            form_cliente.save()
+        return redirect("cliente")
+
+
+@login_required
+def excluir_cliente(request, id_cliente):
     cliente = Cadastro_cliente.objects.get(pk=id_cliente)
-    if request.method =='POST':
-         cliente.delete()
-         return redirect('cliente')
+    if request.method == "POST":
+        cliente.delete()
+        return redirect("cliente")
     # Aqui no item deve-se escolher e passar o que deseja referneciar do banco de dados, ex.: nome, idade etc
     # na página html, neste caso confirmar_exclusao.html
     #  <h1 style="text-align:center;">Confirmar exclusão do Cliente, {{item.nome_cliente}} ? </h1>
-    return render (request, 'cad_clientes/exclusao_cliente.html', {'item': cliente}
-    )
+    return render(request, "cad_clientes/exclusao_cliente.html", {"item": cliente})
 
-#---------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------
 # LÓGICA DE CADASTRO DOS PETS
-#---------------------------------------------------------------------------------
-def pet (request):
-    dados_pet = {
-        'dados_pet': Cadastro_pet.objects.all()
-    }
-    return render (request, 'cad_pets/pets.html',context=dados_pet)
+# ---------------------------------------------------------------------------------
+def pet(request):
+    dados_pet = {"dados_pet": Cadastro_pet.objects.all()}
+    return render(request, "cad_pets/pets.html", context=dados_pet)
 
-def detalhe_pet (request ,id_pet):
-    dados_p = {
-        'dados_p':Cadastro_pet.objects.get(pk=id_pet)
-    }
-    return render (request,'cad_pets/detalhe_pet.html', dados_p)
+
+def detalhe_pet(request, id_pet):
+    dados_p = {"dados_p": Cadastro_pet.objects.get(pk=id_pet)}
+    return render(request, "cad_pets/detalhe_pet.html", dados_p)
+
 
 @login_required
-def criar_pet (request):
-    if request.method == 'POST':
+def criar_pet(request):
+    if request.method == "POST":
         form_pet = CadastroPetForm(request.POST)
         if form_pet.is_valid():
             form_pet.save()
-            return redirect('pet')
+            return redirect("pet")
     else:
         pet_form = CadastroPetForm()
-        form_pet = {
-        'form_pet': pet_form             
-        }
-        return render (request, 'cad_pets/novo_pet.html', context=form_pet)
+        form_pet = {"form_pet": pet_form}
+    return render(request, "cad_pets/novo_pet.html", context=form_pet)
 
-@login_required    
-def editar_pet(request,id_pet):
+
+@login_required
+def editar_pet(request, id_pet):
     pet = Cadastro_pet.objects.get(pk=id_pet)
-            #novo_cadastro/1 -> GET
-    if request.method == 'GET':
-            form_pet = CadastroPetForm(instance=pet)
-            return render (request, 'cad_pets/novo_pet.html', {'form_pet': form_pet})
-            #caso requisição seja POST
+    # novo_cadastro/1 -> GET
+    if request.method == "GET":
+        form_pet = CadastroPetForm(instance=pet)
+        return render(request, "cad_pets/novo_pet.html", {"form_pet": form_pet})
+        # caso requisição seja POST
     else:
-        form_pet =  CadastroPetForm(request.POST, instance=pet)
+        form_pet = CadastroPetForm(request.POST, instance=pet)
         if form_pet.is_valid():
-               form_pet.save()
-        return redirect('pet') 
-    
-@login_required    
-def excluir_pet (request,id_pet):
+            form_pet.save()
+    return redirect("pet")
+
+
+@login_required
+def excluir_pet(request, id_pet):
     pet = Cadastro_pet.objects.get(pk=id_pet)
-    if request.method =='POST':
-         pet.delete()
-         return redirect('pet')
+    if request.method == "POST":
+        pet.delete()
+        return redirect("pet")
     # Aqui no item deve-se escolher e passar o que deseja referneciar do banco de dados, ex.: nome, idade etc
     # na página html, neste caso confirmar_exclusao.html
     #  <h1 style="text-align:center;">Confirmar exclusão do Cliente, {{item.nome_cliente}} ? </h1>
-    return render (request, 'cad_pets/exclusao_pet.html', {'item': pet}
-    )
+    return render(request, "cad_pets/exclusao_pet.html", {"item": pet})
 
-#---------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------
 # LÓGICA DE CADASTRO DAS VENDAS/SERVIÇOS
-#---------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------
 
 
-def venda (request):
-    dados_venda = {
-        'dados_venda': Cadastro_venda.objects.all()
-    }
-    return render (request, 'cad_vendas/vendas.html', context=dados_venda)
+def venda(request):
+    dados_venda = {"dados_venda": Cadastro_venda.objects.all()}
+    return render(request, "cad_vendas/vendas.html", context=dados_venda)
 
-def detalhe_venda (request ,id_venda):
-    dados_v = {
-        'dados_v':Cadastro_venda.objects.get(pk=id_venda)
-    }
-    return render (request,'cad_vendas/detalhe_venda.html', dados_v)
+
+def detalhe_venda(request, id_venda):
+    dados_v = {"dados_v": Cadastro_venda.objects.get(pk=id_venda)}
+    return render(request, "cad_vendas/detalhe_venda.html", dados_v)
+
 
 @login_required
-def criar_venda (request):
-    if request.method == 'POST':
+def criar_venda(request):
+    if request.method == "POST":
         form_venda = CadastroVendaForm(request.POST)
         if form_venda.is_valid():
             form_venda.save()
-            return redirect('venda')
+            return redirect("venda")
     else:
         venda_form = CadastroVendaForm()
-        form_venda = {
-        'form_venda': venda_form             
-        }
-        return render (request, 'cad_vendas/nova_venda.html', context=form_venda)    
+        form_venda = {"form_venda": venda_form}
+    return render(request, "cad_vendas/nova_venda.html", context=form_venda)
 
-@login_required    
-def editar_venda(request,id_venda):
+
+@login_required
+def editar_venda(request, id_venda):
     venda = Cadastro_venda.objects.get(pk=id_venda)
-            #novo_cadastro/1 -> GET
-    if request.method == 'GET':
-            form_venda = CadastroVendaForm(instance=venda)
-            return render (request, 'cad_vendas/nova_venda.html', {'form_venda': form_venda})
-            #caso requisição seja POST
+    # novo_cadastro/1 -> GET
+    if request.method == "GET":
+        form_venda = CadastroVendaForm(instance=venda)
+        return render(request, "cad_vendas/nova_venda.html", {"form_venda": form_venda})
+        # caso requisição seja POST
     else:
-        form_venda =  CadastroVendaForm(request.POST, instance=venda)
+        form_venda = CadastroVendaForm(request.POST, instance=venda)
         if form_venda.is_valid():
-               form_venda.save()
-        return redirect('venda') 
-    
-@login_required    
-def excluir_venda (request,id_venda):
+            form_venda.save()
+    return redirect("venda")
+
+
+@login_required
+def excluir_venda(request, id_venda):
     venda = Cadastro_venda.objects.get(pk=id_venda)
-    if request.method =='POST':
-         venda.delete()
-         return redirect('venda')
+    if request.method == "POST":
+        venda.delete()
+        return redirect("venda")
     # Aqui no item deve-se escolher e passar o que deseja referênciar do banco de dados, ex.: nome, idade etc
     # na página html, neste caso confirmar_exclusao.html
     #  <h1 style="text-align:center;">Confirmar exclusão do Cliente, {{item.nome_cliente}} ? </h1>
-    return render (request, 'cad_vendas/exclusao_venda.html', {'item': venda}
-    )
+    return render(request, "cad_vendas/exclusao_venda.html", {"item": venda})
